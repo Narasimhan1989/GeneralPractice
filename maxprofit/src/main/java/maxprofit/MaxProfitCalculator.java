9fbde16ef55b9cd39ba52c897e484190f895e98f
Narasimhan <narasimhan.sheshadri@happiestminds.com>
2019-04-22 12:33:28 +0530

    commiting class template

---
+++ maxprofit/src/main/java/maxprofit/MaxProfitCalculator.java
@@ -0,0 +1,17 @@
+package maxprofit;
+
+public class MaxProfitCalculator {
+
+	public static void main(String[] args) {
+		
+		int[] prices = {5, 11,3, 50,60,90};
+		System.out.println(maxProfitWithKTransaction(prices,2));
+	}
+	
+	public static int maxProfitWithKTransaction(int [] prices, int k) {
+		
+		
+		return 0;
+	}
+
+}
